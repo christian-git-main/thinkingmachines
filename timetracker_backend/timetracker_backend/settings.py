3b3ec1c36c4c22d043f1a85a341861dd436a5398
Christian Banzon <christian@digibuild.com>
2024-02-20 02:15:28 +0800

    additional things

--- timetracker_backend/timetracker_backend/settings.py
+++ timetracker_backend/timetracker_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+96(561e$!yfbv8a5p$u774fx73ez%^%_s1w&y7jrjb%^sbg09'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -138,8 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
